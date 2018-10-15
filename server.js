a109b7fa0572dedceede8d47c239694f1361fb43
wuyw <wuyw@zonekey.com.cn>
2018-10-15 13:33:50 +0800

    koa-socket连接

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ const Koa = require('koa');
 const path = require('path');
 const koaSend = require('koa-send');
 const static = require('koa-static');
+const socket = require('koa-socket');
+const io = new socket({
+    ioOptions: {
+        pingTimeout: 10000,
+        pingInterval: 5000,
+    }
+});
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
+// socket注入应用
+io.attach(app);
 
 app.use(static(
     path.join( __dirname,  './public')
@@ -29,9 +38,20 @@ app.use(async (ctx, next) => {
         await next();
     }
 });
+io.on('random', ctx=>{ // event data socket.id
+    console.log('value',ctx.data);
+    ctx.socket.emit('warn', ctx.data);
+});
+app.io.on('connection', (ctx) => {
+    console.log(`connection id =>${ctx.socket.id} ip =>${ctx.socket.request.connection.remoteAddress}`);
+});
+app.io.on('disconnect', (ctx) => {
+    console.log(`disconnect id =>${ctx.socket.id}`);
+});
 
 // 在端口3001监听:
-app.listen(3000, _ => {
-    console.log('app started at port 3000...');
+let port = 3001;
+app.listen(port, _ => {
+    console.log('app started at port ...' + port);
 });
 // https.createServer(app.callback()).listen(3001);
