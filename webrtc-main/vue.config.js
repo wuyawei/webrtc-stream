f3d8b87693ead5df61ac5fef3ffab4950bb45669
wuyw <w2499115366@126.com>
2019-03-17 11:48:29 +0800

    在线部署

--- webrtc-main/vue.config.js
+++ webrtc-main/vue.config.js
@@ -8,7 +8,7 @@
 // 基础路径 注意发布之前要先修改这里
 let baseUrl = '/';
 module.exports = {
-    baseUrl: baseUrl, // 根据你的实际情况更改这里
+    publicPath: baseUrl, // 根据你的实际情况更改这里
     lintOnSave: true,
     devServer: {
         publicPath: baseUrl, // 和 baseUrl 保持一致
