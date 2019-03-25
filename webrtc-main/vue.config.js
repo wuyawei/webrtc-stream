3d5186e3f2f51e0a0d8c2152e610b33a88f88e38
wuyw <wuyw@zonekey.com.cn>
2019-03-25 14:37:53 +0800

    打包优化

--- webrtc-main/vue.config.js
+++ webrtc-main/vue.config.js
@@ -10,10 +10,14 @@ let baseUrl = '/';
 module.exports = {
     publicPath: baseUrl, // 根据你的实际情况更改这里
     lintOnSave: true,
+    outputDir: '../public', // 默认 dist 打包文件输出位置
+    assetsDir: 'static', // 打包的 js、css、font 等存放的文件夹路径 相对于 outputDir，默认 ''，直接放在 outputDir里
+    indexPath: '../index.html', // index.html 输出路径 默认 index.html
     devServer: {
         publicPath: baseUrl, // 和 baseUrl 保持一致
         proxy: {
             '/': {
+                ws: false, // websocket 不需要代理
                 target: 'http://localhost:3001',
                 changeOrigin: true
             }
